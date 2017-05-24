def7383c13ad19dde777c9b26131ca3d3dd4aeb1
legarciiaa <leeticia.garcia@gmail.com>
2017-05-23 23:57:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 
 //queue to bag of task
-typedef struct Node{
+struct Node{
     struct FractalIndex *index;
     struct Node *next;
-} BagTask;
+};
+
+struct BagTask{
+   struct Node *first;
+   struct Node *last;
+};
 
 struct FractalIndex{
     double xmin;
@@ -15,8 +20,10 @@ struct FractalIndex{
     double ymax;
 };
 
-BagTask *first = NULL;
-BagTask *last = NULL;
+//start the list
+struct BagTask *work= NULL;
+struct BagTask *result = NULL;
+
 
 void workFractal(){
     //https://gist.github.com/andrejbauer/7919569
@@ -32,16 +39,17 @@ void drawFractal(){
 
 }
 
-void insertQueue(int size){
+void insertWork(int size){
     //https://www.tutorialspoint.com/data_structures_algorithms/linked_list_program_in_c.htm --LINKED LIST
     int i=0;
-    BagTask *current= (BagTask*)malloc(sizeof(BagTask));
-    current ->next =NULL;
+    int count=0;
+    struct Node *node=NULL;
+    struct FractalIndex *fractal=NULL;
 
     //add to list of task
     while (size > 0 ){
-       current->index->xmin=i;
-       current->index->ymin=i;
+       fractal[count].xmin=i;
+       fractal[count].ymin=i;
 
        if (size < i+20){
            i=i+20;
@@ -50,25 +58,26 @@ void insertQueue(int size){
            i=size;
            size = 0;
        }
-       current->index->xmax=i;
-       current->index->ymax=i;
-       }
+       fractal[count].xmax=i;
+       fractal[count].ymax=i;
+       node[count].index= &fractal[count];
 
        //FIFO order
-       if(first== NULL){
-          first= last= current;
+       if(work->first== NULL){
+          work->first= work->last= &node[count];
        }else{
-          last->next=current;
-          last=current;
+          work->last->next= &node[count];
        }
+       count++;
+    }
 }
 
-void deleteQueue(BagTask *task){
+void deleteQueue(struct BagTask *bag, struct Node *task){
     //start from the first link
-   struct BagTask* current = first;
-   struct BagTask* previous = NULL;
+   struct Node* current = bag->first;
+   struct Node* previous = NULL;
 
-   if(first != NULL) {
+   if(bag->first != NULL) {
       //navigate through list
       while(current != task) {
         if(current->next == NULL) {
@@ -80,7 +89,7 @@ void deleteQueue(BagTask *task){
      }
       //found a match, update the link
       if(current == task) {
-         firsts= first->next;
+         bag->first = bag->first->next;
        } else {
          previous->next = current->next;
        }
@@ -89,16 +98,19 @@ void deleteQueue(BagTask *task){
 }
 
 int main(int argc, char *argv[]) {
-    int nColor = argv[1];
-    int nThreads = argv[2];
-    int size= argv[3]; //fractal size total
+    int nColor = atoi(argv[1]);
+    int nThreads = atoi(argv[2]);
+    int size= atoi(argv[3]); //fractal size total
 
-    //add tasks
-    insertQueue(size);
+    if (size > 20){
+        //add tasks
+        insertWork(size);
 
-    //create the thread to work
-    workFractal(nThreads);
+        //create the thread to work
+        workFractal(nThreads);
 
-    //create the thread to draw
-    drawFractal(nThreads);
+        //create the thread to draw
+        drawFractal(nThreads);
+    }
+    return 0;
 }
