8e37a9e889f5cb2487d6385e42094eabee0f1a92
Renata Argerique <renatapva@gmail.com>
2017-06-02 02:47:59 -0300

    aaa

--- main.c
+++ main.c
@@ -24,17 +24,17 @@ struct BagTask{
 };
 
 //start the list
-struct BagTask work;
+//struct BagTask work;
 struct BagTask result;
 int size= 0;
 int max_iter = 100;
 static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t m;
 static pthread_cond_t condWork;
 static pthread_cond_t condResult;
 int taskSize = 20;
 
-void mandelbrot(void *fractal)
-{
+void mandelbrot(void *fractal){
    //http://libxbgi.sourceforge.net/mandelbrot.c
    //http://www.codewithc.com/how-to-include-graphics-h-in-codeblocks/
   /*
@@ -86,27 +86,26 @@ void mandelbrot(void *fractal)
 
 }
 
-void workFractal(){
+void *workFractal(void *params){
     //https://gist.github.com/andrejbauer/7919569
     //Thread consumer, work in fractal and remove task in queue
 
 	//use while() (exemplo righi)
-	struct Node *node= work.first;
+	//struct Node *node= work.first;
 
     while(!condWork){
         //wait for the condition
         pthread_mutex_lock(&m);
-            pthread_cond_wait(&condWork, &m);
-            while(work.first != NULL){
-                printf("\nWORKING IN: Min x: %d Min y: %d Max x: %d Max y: %d", node->index.xmin, node->index.ymin, node->index.xmax, node->index.ymax);
-                printf("\NEXT IN: Min x: %d Min y: %d Max x: %d Max y: %d", node->next->index.xmin, node->next->index.ymin, node->next->index.xmax, node->next->index.ymax);
-		'------	 VERIFICAR NEXT N ESTA CERTO...LOOP 
-                //deleteQueue(work,node);
-                mandelbrot(node);
-                node = work.first = &node->next;
-                //node= work.first;
-              pthread_mutex_unlock(&m);
-            }
+        pthread_cond_wait(&condWork, &m);
+        /*while(work.first != NULL){
+        	//pthread_mutex_lock(&m);
+            //printf("\nWORKING IN: Min x: %d Min y: %d Max x: %d Max y: %d", work.first->index.xmin, work.first->index.ymin, work.first->index.xmax, work.first->index.ymax);
+            //printf("\nFirst work: Min x: %d Min y: %d Max x: %d Max y: %d", work.first->index.xmin, work.first->index.ymin, work.first->index.xmax, work.first->index.ymax);
+            //printf("\nNEXT IN: Min x: %d Min y: %d Max x: %d Max y: %d", work.first->next->index.xmin, work.first->next->index.ymin, work.first->next->index.xmax, work.first->next->index.ymax);
+            mandelbrot(work.first);
+			deleteQueue(work,work.first);
+          	pthread_mutex_unlock(&m);
+        }*/
     }
 
 }
@@ -116,7 +115,7 @@ void drawFractal(){
     //use while() (exemplo righi)
 
    struct Node *node= result.first;
-   while(!condResult){
+   while(condResult){
         //wait for the condition
         pthread_cond_wait(&condResult, &m);
         while(result.first != NULL){
@@ -128,57 +127,49 @@ void drawFractal(){
     }
 }
 
-void insertWork(){int nextIndex=0;
+void insertWork(struct BagTask *work){
+	int nextIndex=0;
     int sizeTask=size;
     struct Node node;
     struct Node *currentNode;
-    struct FractalIndex *fractal= (struct FractalIndex*)(malloc(sizeof(struct FractalIndex)));
-    int i=0;
-
+    struct FractalIndex fractal = {0, 0, 0, 0};
+    
     //add work to list of tasks and remove from original size
     while (sizeTask > 0 ){
-       printf("\nCreate job. Updated original size: %d Next index: %d",sizeTask,nextIndex);
-       fractal[i].xmin=fractal[i].ymin=nextIndex;
+       printf("\nCreate job. Updated original size: %d Next index: %d", sizeTask, nextIndex);
+       fractal.xmin=fractal.ymin=nextIndex;
 
        if (sizeTask > 0){
-           fractal[i].xmax=fractal[i].ymax=nextIndex=nextIndex+(taskSize-1);
+           fractal.xmax=fractal.ymax=nextIndex=nextIndex+(taskSize-1);
            sizeTask = sizeTask-taskSize;
        }else{
-           fractal[i].xmax=fractal[i].ymax=sizeTask;
+           fractal.xmax=fractal.ymax=sizeTask;
            sizeTask = 0;
        }
        nextIndex++;
-       i++;
-
-       //only sets the index of the current node if this is the first one
-       //printf("\nFRACTAL: Min x: %d Min y: %d Max x: %d Max y: %d", fractal.xmin, fractal.ymin, fractal.xmax, fractal.ymax);
-
-       if(work.first == NULL){
-            node.index = fractal[i];
-       		work.first = &node;
+       
+       if(work->first == NULL){
+            node.index = fractal;
+            node.next = NULL;
+       		work->first = &node;
        		currentNode = &node;
-       		work.last = &node;
+       		work->last = &node;
 		} else{
 			//sets the next node as the newly calculated index
 			struct Node tempNode;
-			tempNode.index = fractal[i];
+			tempNode.index = fractal;
 			currentNode->next = &tempNode;
 			currentNode = &tempNode;
-			work.last = &tempNode;
+			work->last = &tempNode;
 		}
 
-		//This is just to test if it works, we can remove it
-		//printf("\nFirst work: Min x: %d Min y: %d Max x: %d Max y: %d", work.first->index.xmin, work.first->index.ymin, work.first->index.xmax, work.first->index.ymax);
-		//if(work.first->next != NULL){
-		//	printf("\nSecond work: Min x: %d Min y: %d Max x: %d Max y: %d", work.first->next->index.xmin, work.first->next->index.ymin, work.first->next->index.xmax, work.first->next->index.ymax);
-		//}
-
-         printf("\nFIRST: Min x: %d Min y: %d Max x: %d Max y: %d",  &work.first->index.xmin,  &work.first->index.ymin,  &work.first->index.xmax,  &work.first->index.ymax);
-
-		//wake condition condWork
-		printf("\nWake up condition work");
-		pthread_cond_signal(&condWork);
+        //pthread_cond_signal(&condWork);
     }
+    
+    //wake condition condWork
+	//	printf("\nWake up condition work");
+	//pthread_cond_init(&condWork, NULL);
+	//pthread_cond_signal(&condWork);
 }
 
 void deleteQueue(struct BagTask *bag, struct Node task){
@@ -210,24 +201,31 @@ int main(int argc, char *argv[]) {
     int nThreads = atoi(argv[1]);
     int nColor = atoi(argv[2]);
     size = atoi(argv[3]); //fractal size total
+    int dummyArg = 0;
+    struct BagTask bagTask = {NULL, NULL};
+    struct BagTask *work = &bagTask;
 
     int threadCounter;
     pthread_t threadId[nThreads];
+    pthread_mutex_init(&m, NULL);
 
 	//if the total size of the fractal is bigger than the bucket size can perform the algorithm
     if (size > taskSize){
         //separate image in tasks and add them to bucket
-        insertWork();
-
+        insertWork(work);
+        
         //create the threads to work and draw
 		for(threadCounter=0; threadCounter<nThreads; threadCounter++){
-			pthread_create(&threadId[threadCounter], NULL, workFractal,NULL);
-			pthread_create(&threadId[threadCounter], NULL, drawFractal, NULL);
+			printf("\nEnter thread for.");
+			printf("\nFirst work: Min x: %d Min y: %d Max x: %d Max y: %d", bagTask.first->index.xmin, work->first->index.ymin, work->first->index.xmax, work->first->index.ymax);
+			pthread_create(&threadId[threadCounter], NULL, workFractal, &dummyArg);
+			//pthread_create(&threadId[threadCounter], NULL, drawFractal, NULL);
 		}
 
-		for(threadCounter=0; threadCounter<nThreads; threadCounter++){
+	/*	for(threadCounter=0; threadCounter<nThreads; threadCounter++){
 			pthread_join(threadId[threadCounter], NULL);
-		}
+			//pthread_join(threadId[threadCounter], NULL);
+		}*/
     }
     return 0;
 }
